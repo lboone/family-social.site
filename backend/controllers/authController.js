dd91a69be6a777c23106e280af2a3cba4bce4c10
Lloyd Boone <lboone@progressive.com>
2025-07-07 17:33:35 -0400

    implement account verification and OTP resend functionality

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -97,3 +97,83 @@ exports.signup = catchAsync(async (req, res, next) => {
     );
   }
 });
+
+exports.verifyAccount = catchAsync(async (req, res, next) => {
+  const { otp } = req.body;
+  if (!otp) {
+    return next(new AppError("OTP is required for verification.", 400));
+  }
+
+  const user = req.user;
+  console.log({ user });
+  if (user.otp !== otp) {
+    return next(new AppError("Invalid OTP. Please try again.", 400));
+  }
+
+  if (Date.now() > user.otpExpires) {
+    return next(
+      new AppError("OTP has expired. Please request a new one.", 400)
+    );
+  }
+
+  user.isVerified = true;
+  user.otp = undefined; // Clear OTP after verification
+  user.otpExpires = undefined; // Clear OTP expiration
+
+  await user.save({ validateBeforeSave: false });
+
+  createSendToken(
+    user,
+    200,
+    res,
+    "Account verified successfully! You can now log in."
+  );
+});
+
+exports.resendOtp = catchAsync(async (req, res, next) => {
+  const { email } = req.user;
+
+  if (!email) {
+    return next(new AppError("Email is required", 404));
+  }
+
+  const user = await User.findOne({ email });
+  if (!user) {
+    return next(new AppError("User Not Found!", 404));
+  }
+  if (user.isVerified) {
+    return next(new AppError("Account already verified", 400));
+  }
+
+  user.otp = generateOtp();
+  user.otpExpires = Date.now() + 24 * 60 * 60 * 1000; // OTP valid for 24 hours
+  await user.save({ validateBeforeSave: false });
+
+  const htmlTemplate = loadTemplate("otpTemplate.hbs", {
+    title: "Otp Verification",
+    username: user.username,
+    otp: user.otp,
+    message: "Your one-time password (OTP) for account verification is:",
+  });
+
+  try {
+    await sendEmail({
+      email: user.email,
+      subject: "Resend OTP for Email Verification",
+      html: htmlTemplate,
+    });
+
+    res.status(200).json({
+      status: "success",
+      message: "New OTP sent to your email.",
+    });
+  } catch (error) {
+    user.otp = undefined; // Clear OTP if email sending fails
+    user.otpExpires = undefined; // Clear OTP expiration
+    await user.save({ validateBeforeSave: false });
+    console.error("Error sending OTP email:", error);
+    return next(
+      new AppError("Failed to resend OTP. Please try again later.", 500)
+    );
+  }
+});
--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,8 +1,15 @@
 const express = require("express");
-const { signup } = require("../controllers/authController");
+const {
+  signup,
+  verifyAccount,
+  resendOtp,
+} = require("../controllers/authController");
+const isAuthenticated = require("../middleware/isAuthenticated");
 
 const router = express.Router();
 
 router.post("/signup", signup);
+router.post("/verify", isAuthenticated, verifyAccount);
+router.post("/resend-otp", isAuthenticated, resendOtp);
 
 module.exports = router;
